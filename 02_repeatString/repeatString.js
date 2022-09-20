0829b5193bc4f8d1a477ac55b28f504d87c9c7d6
CabeLovlie <cabeklovlie@gmail.com>
2022-09-20 14:18:28 -0700

    Solves the first 2 exercises
    My solutions to the first two TOP JavaScript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, num) {
+    let repeatedString = '';
 
+    if (num < 0) {
+        repeatedString = 'ERROR';
+    } else {
+        for (i = 0; i < num; i++) {
+            repeatedString += stringToRepeat;
+        }
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
